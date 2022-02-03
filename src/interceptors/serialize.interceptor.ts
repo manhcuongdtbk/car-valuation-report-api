a9a5449d70ee274ab76e03e6c5b288cd9d95e9da
manhcuongdtbk <manhcuongdtbk@gmail.com>
2022-02-03 18:46:57 +0700

    Add custom interceptor to serialize response user data

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,41 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+import { UserDto } from 'src/users/dtos/user.dto';
+
+interface ClassConstructor {
+  // This interface acts as a Class
+  new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    // Run something before a request is handled by the request handler
+    // console.log('This is running before the handler', context);
+
+    return next.handle().pipe(
+      map((data: any) => {
+        // Run something before the response is sent out
+        // console.log('This is running after the handler (before the response is sent out), data)
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
