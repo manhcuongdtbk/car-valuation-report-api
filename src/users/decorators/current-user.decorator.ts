aff0ceba5439e69e1e50151160539e74b215723b
manhcuongdtbk <manhcuongdtbk@gmail.com>
2022-02-12 00:22:14 +0700

    Use global CurrentUserInterceptor and CurrentUser decorator to custom the param for whoami route

---
+++ src/users/decorators/current-user.decorator.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const CurrentUser = createParamDecorator(
+  (data: never, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+    // currentUser is assigned in the CurrentUserInterceptor
+    return request.currentUser;
+  },
+);
